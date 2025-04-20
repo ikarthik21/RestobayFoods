3a14f3c58fe20e29c2ffb75c83a130e3c11c684b
ikarthik21 <karthikeyakallapu@gmail.com>
2025-04-20 18:39:25 +0530

    added left margin for BlockWrapper

--- client/src/_components/Wrappers/BlockWrapper.jsx
+++ client/src/_components/Wrappers/BlockWrapper.jsx
@@ -2,7 +2,7 @@
 const BlockWrapper = ({ children, rounded, background, margin, padding }) => {
   return (
     <div
-      className={`rounded-xl ml-4 md:ml-12 mt-4  p-4 md:mt-12 ${margin} ${rounded} ${padding}  `}
+      className={`rounded-xl ml-4 md:ml-16 mt-4  p-4 md:mt-12 ${margin} ${rounded} ${padding}  `}
       style={{ backgroundColor: background || "#fff0df" }}
     >
       {children}
